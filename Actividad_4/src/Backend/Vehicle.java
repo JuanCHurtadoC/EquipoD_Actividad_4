1aaeb7c88d05965fb9f6c33e7ffc256053539b2f
JcamiloHC <juanhurtado1126030@correo.itm.edu.co>
2024-11-02 14:56:00 -0500

    Se crea proyecto de actividad 4

---
+++ Actividad_4/src/Backend/Vehicle.java
@@ -0,0 +1,7 @@
+package Backend;
+
+
+abstract class Vehicle {
+    
+    
+}
---
+++ Actividad_4/src/actividad_4/Actividad_4.java
@@ -0,0 +1,9 @@
+package actividad_4;
+
+
+public class Actividad_4 {
+    
+    public static void main(String[] args) {
+    }
+    
+}
