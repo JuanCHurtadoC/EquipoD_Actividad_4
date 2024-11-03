c52fa84ce4b510568ed79d5ef31ab3b1b887d4ad
JcamiloHC <juanhurtado1126030@correo.itm.edu.co>
2024-11-03 11:56:11 -0500

    Se agregan las interfaces de EngineLand y EngineWater, ademas se crea la clase Land

---
+++ Actividad_4/src/Backend/Land.java
@@ -0,0 +1,31 @@
+
+package Backend;
+
+
+public class Land extends Vehicle implements EngineLand {
+    
+    private String type;
+    private City city;
+
+    public Land(int power, int year, String brand, String type, City city) {
+        super(power, year, brand);
+        this.type = type;
+        this.city = city;
+    }
+    public Land(String type, City city) {
+    super(0,0,"");
+    this.type = type;
+    this.city = city;
+    }
+
+    @Override
+    public String description() {
+        return "vehiculo terrestre: " + brand + " (" + year + "), Tipo: " + type;
+    }
+
+    @Override
+    public String moveWheels() {
+        return "El vehiculo se mueve con ruedas.";
+    }
+     
+}
