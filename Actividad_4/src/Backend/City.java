bef3d546abfd4f68be241cbc494379190210d988
AngelDavidGutierrez <angelgutierrez1132998@correo.itm.edu.co>
2024-11-02 19:25:58 -0500

    Update City.java
    no se porque no aparecia el codigo

---
+++ Actividad_4/src/Backend/City.java
@@ -0,0 +1,21 @@
+public class City {
+    private String name;
+    private String code;
+
+    public City(String name, String code) {
+        this.name = name;
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String toString() {
+        return "Ciudad: Nombre:'" + name + "', codigo:'" + code + "'";
+    }
+}
